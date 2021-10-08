ed5b9e5bc0aff52ee9286e8a2d825f3ca2fd8d7b
sammy van cleve <sammyvancleve@gmail.com>
2021-10-08 11:18:30 -0400

    add simple api call for sempre query

--- foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/FoiaSpringApiApplication.java
+++ foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/FoiaSpringApiApplication.java
@@ -1,5 +1,6 @@
 package edu.middlebury.foiaspringapi;
 
+import org.jsoup.Jsoup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/SempreQueryController.java
+++ foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/SempreQueryController.java
@@ -1,15 +1,31 @@
 package edu.middlebury.foiaspringapi;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class SempreQueryController {
 	
-	//@GetMapping("/sempre")
-	/*
-	public SempreQuery query(@RequestParam(value="query", defaultValue = "") String query) {
-		
+	@GetMapping("/sempre")
+	public SempreQuery query(@RequestParam(value="query", defaultValue = "") String query) throws IOException {
+		String url = "http://localhost:8400/sempre?q=";
+		String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8.toString());
+		Document doc = Jsoup.connect(url + encodedQuery).get();
+		Elements content = doc.getElementsByClass("answer");
+		String response = null;
+		for (Element answer : content) {
+			response = answer.text();
+		}
+		SempreQuery result = new SempreQuery(query);
+		result.setResponse(response);
+		return result;
 	}
-	(*/
 
 }
