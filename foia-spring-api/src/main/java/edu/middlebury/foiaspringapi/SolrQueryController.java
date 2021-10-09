0b2a4ca211ce3b8580aaede26984a3e2a64e3dbb
Jake Gaughan <mgaughan@middlebury.edu>
2021-10-08 21:33:39 -0400

    ERROR: NPE on request to Solr

--- foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/SolrQueryController.java
+++ foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/SolrQueryController.java
@@ -1,5 +1,6 @@
 package edu.middlebury.foiaspringapi;
 
+//import org.apache.http.client.HttpClient;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,23 +10,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.lang.ProcessBuilder;
 import java.lang.Process;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 @RequestMapping("/api")
 public class SolrQueryController {
 
     @GetMapping("/solr")
-    public String solr(@RequestParam(value = "q", defaultValue = "") String query) throws IOException {
-        System.out.print("hi");
-        String command = "curl 'http://localhost:8983/solr/vtstatefiles/select?q=*:*'";
-        Process process = Runtime.getRuntime().exec(command);
-        System.out.println(process.getInputStream());
-        return (command);
+    public String solr(@RequestParam(value = "q", defaultValue = "*:*") String query) throws IOException {
+        /*
+         * DEBUG: set up solr using A dataset need to name the index 'vtstatefiles' run
+         * on port 8983 on request, ERROR 64104: null pointer exception
+         */
+
+        // String command = "curl 'http://localhost:8983/solr/vtstatefiles/select?q='";
+        String url = "http://localhost:8983/solr/vtstatefiles/select?q=*:*";
+        Document doc = Jsoup.connect(url).ignoreContentType(true).get();
+        Element pre = doc.select("pre").first();
+        return pre.text();
+
     }
 
 }
