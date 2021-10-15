fdc2f99770ec2c938a93bc840a879861bf38fc77
Jake Gaughan <mgaughan@middlebury.edu>
2021-10-14 21:44:41 -0400

    http request works

--- foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/SolrQueryController.java
+++ foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/SolrQueryController.java
@@ -10,52 +10,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.micrometer.core.ipc.http.HttpSender.Response;
-
 import java.io.IOException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.lang.ProcessBuilder;
-import java.lang.Process;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.apache.coyote.Request;
-import org.apache.http.HttpResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("/api")
 public class SolrQueryController {
 
     @GetMapping("/solr")
-    public String solr(@RequestParam(value = "q", defaultValue = "*:*") String query) throws IOException {
-        String url = "http://localhost:8983/solr/vtstatefiles/select?q=";
-        Document doc = Jsoup.connect(url + query).ignoreContentType(true).get();
-        Element body = doc.select("body").first();
+    public StringBuffer solr(@RequestParam(value = "q", defaultValue = "*:*") String query) throws IOException {
+        String url0 = "http://localhost:8983/solr/vtstatefiles/select?q=";
+        // Document doc = Jsoup.connect(url + query).ignoreContentType(true).get();
+        // Element body = doc.select("body").first();
 
-        String command = "curl -X GET http://localhost:8983/solr/vtstatefiles/select?q=" + query;
-        // Process process = Runtime.getRuntime().exec(command);
-        // System.out.println(process.getInputStream());
-        // process.destroy();
-        // ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
-        // processBuilder.directory(new File("/home/"));
-        // Process process = processBuilder.start();
-
-        // URL url0 = new URL((url + query));
-        // HttpURLConnection http = (HttpURLConnection) url0.openConnection();
-        // http.setRequestProperty("Accept", "application/json");
+        URL url = new URL((url0 + query));
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("GET");
+        http.setRequestProperty("Accept", "application/json");
         // System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
-        // System.out.println(http.getE());
-        // http.disconnect();
-
-        Request request = Request.get("http://localhost:8983/solr/vtstatefiles/select?q=a");
-        HttpResponse httpResponse = request.execute().returnResponse();
-        System.out.println(httpResponse.getStatusLine());
-        if (httpResponse.getEntity() != null) {
-            String html = EntityUtils.toString(httpResponse.getEntity());
-            System.out.println(html);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = reader.readLine()) != null) {
+            if (inputLine.contains("Contact")) {
+                content.append(inputLine);
+            } else if (inputLine.contains("Name")) {
+                content.append(inputLine);
+            }
+            // content.append(inputLine);
         }
+        System.out.println(content);
 
-        return body.text();
+        reader.close();
+        http.disconnect();
+
+        return content;
 
     }
 
