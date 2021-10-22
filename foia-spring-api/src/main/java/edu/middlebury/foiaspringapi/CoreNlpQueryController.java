cfc6359cce60d9a3e7336201dc0f65f711af4d25
Jake Gaughan <mgaughan@middlebury.edu>
2021-10-22 14:42:41 -0400

    error: commented import in CoreNlpQController

--- foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/CoreNlpQueryController.java
+++ foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/CoreNlpQueryController.java
@@ -12,11 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 public class CoreNlpQueryController {
 
     @GetMapping("/corenlp")
     public String query(@RequestParam(value = "q", defaultValue = "") String query) throws IOException {
         return query;
+    }
+
+    public static void coreApiCall(String[] args) {
+        // https://stanfordnlp.github.io/CoreNLP/api.html
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
+        props.setProperty("coref.algorithm", "neural");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        // CoreDocument document = new CoreDocument(text);
     }
 
 }
