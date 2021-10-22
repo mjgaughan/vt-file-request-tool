0f34c9a33e457d06f6a9fa95e8c60b3b17389cbc
Jake Gaughan <mgaughan@middlebury.edu>
2021-10-22 14:11:16 -0400

    initial construction

--- foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/CoreNlpQueryController.java
+++ foia-spring-api/src/main/java/edu/middlebury/foiaspringapi/CoreNlpQueryController.java
@@ -1,5 +1,22 @@
 package edu.middlebury.foiaspringapi;
 
+//import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.ie.util.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.semgraph.*;
+import edu.stanford.nlp.trees.*;
+import java.util.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 public class CoreNlpQueryController {
+
+    @GetMapping("/corenlp")
+    public String query(@RequestParam(value = "q", defaultValue = "") String query) throws IOException {
+        return query;
+    }
 
 }
