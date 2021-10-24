c85afb0d210f581b7ed2f15811cca7dcaeca6c4d
Jake Gaughan <mgaughan@middlebury.edu>
2021-10-24 16:30:31 -0400

    initialized the two angular components

---
+++ vt-files-frontend/src/app/return-list/return-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-return-list',
+  templateUrl: './return-list.component.html',
+  styleUrls: ['./return-list.component.css']
+})
+export class ReturnListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
